94668ff773dd598e66582cd4256c45cd58ab21d5
FiifiAsare <don.dc.carlos@gmail.com>
2022-06-30 16:37:03 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - Prints quote to the standard error
+ * main - Entry point
  *
- * Return: zero on success
- *
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
-return (0);
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
