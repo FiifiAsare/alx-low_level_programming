60c6a4f65cf5d0ec78855640f2bbda6a67c6d82c
FiifiAsare <don.dc.carlos@gmail.com>
2022-06-30 13:03:01 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #!/bin/bash
 #include (Stdio.h)
 int main(void)
-{
-   puts("\""Programming is like building a multilingual puzzle");
+{ 
+ puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
