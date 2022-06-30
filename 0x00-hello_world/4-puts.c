0ffdb1eb636b3c79436c3947ed0bd5575092dce4
FiifiAsare <don.dc.carlos@gmail.com>
2022-06-30 08:26:08 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+#include <Stdio.h>
+
+/**
+ * main -Prints quote
+ * *
+ * Return: zero on success
+ *
+ */
+int main(void)
+
+{
+	puts("\"Programming is like biulding a multilingual puzzle");
+	return (0);
+
+}
