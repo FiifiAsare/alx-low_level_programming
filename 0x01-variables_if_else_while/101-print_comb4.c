6dff67cae78c2d722b447249c9c2b501ccf173c7
FiifiAsare <don.dc.carlos@gmail.com>
2022-07-02 22:10:07 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,7 +15,6 @@ int i, j, k;
 for (i = '0'; i <= '9'; i++)
 {
 for (j = '0'; j <= '9'; j++)
-{
 for (k = '0'; k <= '9'; k++)
 {
 if ((i < j) & (j < k) & (k <= '9'))
@@ -23,13 +22,8 @@ if ((i < j) & (j < k) & (k <= '9'))
 putchar(i);
 putchar(j);
 putchar(k);
-if ((i + j + k) < 168)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
+if ((i + j + k) < 168)													{															putchar(',');														putchar(' ');														}
+}															}	
 }
 putchar('\n');
 return (0);
