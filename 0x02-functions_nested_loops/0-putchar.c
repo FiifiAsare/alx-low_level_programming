360048020b1689945dc237a9830513de56ffdf55
FiifiAsare <don.dc.carlos@gmail.com>
2022-07-05 18:20:27 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,30 +1,21 @@
-#include <stdio.h>
+#include "holberton.h"
+
 /**
- * main - entry point
+ * main - Entry point
  *
- * Return: alway returns 0
+ * Return: Always returns 0
  *
  */
 int main(void)
 {
-int i;
-int j;
-for (i = 0; i <= 99; i++)
-{
-for (j = i + 1; j <= 99; j++)
-{
-putchar(i / 10 + '0');
-putchar(i % 10 + '0');
-putchar(' ');
-putchar(j / 10 + '0');
-putchar(j % 10 + '0');
-if (!(i == 98 && j == 99))
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');;
+	_putchar('\n');
+	return (0);
 }
