ae5ea2f61f6c38189d5433ca0cbe8838f026a9ed
FiifiAsare <don.dc.carlos@gmail.com>
2022-07-07 20:14:54 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * putchar-print numbers 0-9 
+ * main- check the code
+ * return always 0
+ * */
+
+ void print_numbers()void 
+
+{
+int c;
+for (c = '0; c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
