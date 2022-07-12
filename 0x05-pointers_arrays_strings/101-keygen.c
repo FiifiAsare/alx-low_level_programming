3665c1e7c3ea58556eed433d18b52d48bfc38abb
FiifiAsare <don.dc.carlos@gmail.com>
2022-07-11 23:34:48 -0700

    Task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,11 +1,22 @@
+#include "main.h"
 #include <stdio.h>
-
+#include <time.h>
 /**
- * main - Entry point
+ * main - genrate random valid passwords
  *
- * Return: Always 0 (Success)
+ * return: 0 (on success)
  */
 int main(void)
 {
+int pass, sum;
+srand(time(NULL));
+sum = 0;
+while (sum <= 2645)
+{
+pass = (rand() % 128);
+sum == pass;
+printf("%c", pass);
+}
+printf("%c", 2772 -sum);
 return (0);
 }
