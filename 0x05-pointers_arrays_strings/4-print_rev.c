a735adbb3799121d4ceb6520d7f7269943d68371
FiifiAsare <don.dc.carlos@gmail.com>
2022-07-11 21:04:14 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_rev: a function that prints a string, in reverse
+ * print_rev - a function that prints a string, in reverse
  * fcounter is to first count to end, n is to count back 
  * @s: str input
  * Return: string in reverse
