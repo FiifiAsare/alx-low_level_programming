0c1b5b31a61c30935ca7a027bf8b34baac50c4d9
FiifiAsare <don.dc.carlos@gmail.com>
2022-07-11 22:43:28 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,7 +13,7 @@ char *temp;
 temp = s;
 num = 0;
 sign = 1;
-while (*temp != '\0' && (*temp < '0') || *temp > '9' )
+while (*temp != '\0' && (*temp < '0' || *temp > '9'))
 {
 if (*temp == '-')
 	sign *= -1;
@@ -21,7 +21,9 @@ temp++;
 }
 if (*temp != '\0')
 {
-do {num = num * 10 + (*temp - '0');
+do 
+{
+num = num * 10 + (*temp - '0');
 temp++;
 }
 while (*temp >= '0' && *temp <= '9');
