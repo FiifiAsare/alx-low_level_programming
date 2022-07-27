8501b5fa40fbd3987a6cac25471df93523b3d698
FiifiAsare <don.dc.carlos@gmail.com>
2022-07-26 21:21:00 -0700

    Task 7

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -31,3 +31,51 @@ s[i][k + 1] = '\0';
 }
 s[word + 1] = NULL;
 return (s);
+}
+/**
+ * strtow - string to words
+ * @str: string to check
+ * Return: return char value
+ */
+char **strtow(char *str)
+{
+int len, i, j, size, k, word;
+char **s;
+if (str == NULL)
+return (NULL);
+len = 0;
+word = 0;
+while (str[len] != '\0')
+{
+if (str[0] != ' ')
+word++;
+if (str[len] != ' ' && str[len - 1] == ' ' && len != 0)
+word++;
+len++;
+}
+s = (char **)malloc(sizeof(char *) * word + 1);
+if (s == NULL)
+return (NULL);
+j = 0;
+for (i = 0; i < word; i++)
+{
+size = 0;
+for (; j < len; j++)
+{
+if (str[0] != ' ' || str[j] != ' ')
+size++;
+if (str[j] == ' ' && size > 0)
+break;
+}
+printf("%d\n", size);
+s[i] = (char *)malloc(sizeof(char) * size + 1);
+if (s[i] == NULL)
+{
+for (k = i - 1; k >= 0; k--)
+free(s[k]);
+free(s);
+}
+}
+s = helper(word, len, str, s);
+return (s);
+}
