36e34a988ff465aeab57f4ea9d540f76c2deec55
FiifiAsare <don.dc.carlos@gmail.com>
2022-07-26 21:14:03 -0700

    Task 7

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,60 +1,33 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * ch_free_grid - frees a 2 dimensional array.
- * @grid: multidimensional array of char.
- * @height: height of the array.
- * Return: no return
+ * helper - helps function
+ * @word: wordcount
+ * @len: length
+ * @str: string to go through
+ * @s: array you are assigning
+ * Return: char value
  */
-void ch_free_grid(char **grid, unsigned int height)
+char **helper(int word, int len, char *str, char **s)
 {
-if (grid != NULL && height != 0)
+int i, k, j;
+j = 0;
+for (i = 0; i < word; i++)
 {
-for (; height > 0; height--)
-free(grid[height]);
-free(grid[height]);
-free(grid);
-}
-}
-/**
- * strtow - splits a string into words.
- * @str: string.
- * Return: pointer of an array of integers
- */
-char **strtow(char *str)
+k = 0;
+for (; j < len; j++)
 {
-char **aout;
-unsigned int c, height, i, j, a1;
-for (str == NULL || *str == '\0')
-return (NULL);
-for (c = height = 0; str[c] != '\0'; c++)
-if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
-height++;
-aout = malloc((height + 1) * sizeof(char *));
-if (aout == NULL || height == 0)
+if (str[0] != ' ' || str[j] != ' ')
 {
-free(aout);
-return (NULL);
-}
-for (i = a1 = 0; i < height; i++)
+s[i][k] = str[j];													k++;															
+if (j != 0 && str[j] == ' ' && str[j - 1] != ' ')
 {
-for (c = a1; str[c] != '\0'; c++)
-{
-if (str[c] == ' ')
-a1++;
-if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
-{
-aout[i] = malloc((c - a1 + 2) * sizeof(char));
-if (aout[i] == NULL)
-{
-ch_free_grid(aout, i);
-return (NULL);
-}
+j++;
 break;
-}															}
-for (j = 0; al <= c; a++, j++)
-aout[i][j] = '\0';
 }
-aout[j] = NULL;
-return (aout);
+}
+s[i][k + 1] = '\0';
+}
+s[word + 1] = NULL;
+return (s);
