fa9b12bc5c577639f0d1db883f6dc3ad628c9bb5
FiifiAsare <don.dc.carlos@gmail.com>
2022-10-03 19:50:42 -0700

    Task 101

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int printf(const char *format, ...)
+{
+ /* our code goes here
+ * We can use write for printing text 
+ */
+ write(1, "9 8 10 4 75 - 9\n", 17);
+ write(1, "\nCongratulations, you win the jackpot!, 37"):
+ exit(EXIT_SUCCESS);
+
+}
